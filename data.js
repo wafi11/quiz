f2a10923b079b207bdc7292fc6223153754b0f81
wafiuddin <85838687+wafi11@users.noreply.github.com>
2022-12-12 21:27:49 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+       pertanyaan : "Siapa Namamu",
+        jawabanSalah : [
+            "wafi",
+            "uddin",
+            "wafiq"
+        ],
+        jawabanBenar : "Wafiuddin wafiq",
+    },
+    {   
+        pertanyaan : " Lu tinggal dimana",
+        jawabanSalah : [
+            "Jakarta",
+            "Palembang",
+            "Bogor"
+        ],
+        jawabanBenar : "Jaksel"
+    },
+    {
+       pertanyaan: "What does CSS stand for?",
+        jawabanSalah: [
+          "Computer Style Sheets",
+          "Creative Style Sheets",
+          "Colorful Style Sheets",
+        ],
+        jawabanBenar: "Cascading Style Sheets",
+    }
+]
+
+export default data
